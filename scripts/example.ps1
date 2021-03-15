77f53927c942f240c1ca2c3495fe8c63a768148d
Vladimir Murashkin <savgdev@gmail.com>
2021-03-15 12:55:08 +0300

    example script updated to module path

--- scripts/example.ps1
+++ scripts/example.ps1
@@ -1,15 +1,18 @@
-Import-Module   "$PSScriptRoot/../Module/PowerShellMqtt.Listener.dll" -Force
+$module = Import-Module "$PSScriptRoot/../PowerShellMqtt" -Force
 
 $filePath = $PSScriptRoot + "\output.txt"
 $filePath
 
-Start-MqttListener `
--Server "localhost" `
--ClientId "ClientId-001" `
--Port 1883 `
--Username "username" `
--Password "password" `
--Topic 'brand/type/group/code' `
--OnlyPayload `
--CleanSession:$false `
--Verbose > $filePath
+$args = @{
+    Server = "localhost"
+    ClientId = "ClientId-001"
+    Port = 1883
+    Username = "username"
+    Password = "password"
+    Topic = 'brand/type/group/code'
+    OnlyPayload = $true
+    CleanSession = $false
+    Verbose = $true
+}
+
+Start-MqttListener @args | Out-File -FilePath $filePath -Append
